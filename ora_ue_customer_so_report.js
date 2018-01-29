a7d8611985ef5f225d83a87cd05e195790a95f71
ktavo <gariosp@unal.edu.co>
2018-01-29 18:13:17 -0300

    Adding task caller for scheduled script

---
+++ ora_ue_customer_so_report.js
@@ -0,0 +1,36 @@
+/**
+ * @NApiVersion 2.0
+ * @NScriptType UserEventScript
+ * @NModuleScope SameAccount
+ */
+define([
+	'N/task'
+], function(task) {
+     /**
+     * Function definition to be triggered before record is loaded.
+     *
+     * @param {Object} scriptContext
+     * @param {Record} scriptContext.newRecord - New record
+     * @param {Record} scriptContext.oldRecord - Old record
+     * @param {string} scriptContext.type - Trigger type
+     * @Since 2015.2
+     */
+    function afterSubmit(scriptContext) {
+    	var customer = context.newRecord;
+    	var schedTask = task.create({
+    		taskType : task.TaskType.SCHEDULED_SCRIPT
+    	});
+    	
+    	schedTask.scriptId = 'customscript_ora_ss_generate_so_report';
+    	schedTask.deploymentId = 'customdeploy_ora_ss_generate_so_report';
+    	
+    	schedTask.submit();    	
+    	
+
+    }
+
+    return {
+        afterSubmit: afterSubmit
+    };
+    
+});
