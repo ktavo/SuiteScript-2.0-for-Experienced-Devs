2d893a53c34c8a522b095d2713d5a7072325bed3
ktavo <gariosp@unal.edu.co>
2018-02-15 13:07:55 -0300

    Handling user event errors from the error module

--- ora_ue_customer.js
+++ ora_ue_customer.js
@@ -13,7 +13,6 @@ function(recordModule, emailModule, errorHandler) {
     return {
     	afterSubmit: function (context) { 	
     		log.debug('context type: ' , context.type);
-    		errorHandler.customLog();
     		if (context.type == 'edit')
     		{
     			var customer = context.newRecord;
@@ -23,7 +22,7 @@ function(recordModule, emailModule, errorHandler) {
         		try
         		{
         			//Javascript Error
-        			//customerEmail = bla;
+        			customerEmail = bla;
         			var salesRep = recordModule.load({
             			type	: recordModule.Type.EMPLOYEE,
             			//Netsuite error comment 'id : customer.getValue('salesrep'),'
@@ -50,27 +49,7 @@ function(recordModule, emailModule, errorHandler) {
         		}
         		catch(e)
         		{
-        			log.error('Error', '-----There was an error handling the request-----');
-        			log.error('Error', e);
-        			var javascriptError = (e instanceof Error);
-        			if (javascriptError == true)
-        			{
-            			log.error('***JavaScript Error***', '***JavaScript Error***');
-            			log.error('Error name:', e.name);
-            			log.error('Error message:', e.message);
-            			log.error('Error stack:', e.stack);
-            			log.error('Error file name:', e.fileName);
-            			log.error('Error line number:', e.lineNumber);
-        			}
-        			else
-        			{
-            			log.error('***SuiteScript Error***', '***SuiteScript Error***');
-            			log.error('Error name:', e.name);
-            			log.error('Error message:', e.message);
-            			log.error('Error stack:', e.stack);
-            			log.error('Error id:', e.id);
-            			log.error('Error cause:', JSON.stringify(e.cause));
-        			}
+            		errorHandler.customLog(e);
         		}
     		}
     		else
