f1b4187cd6ea1dfa69a3c172ba0f42a0233c4bbb
ktavo <gariosp@unal.edu.co>
2018-02-26 16:16:49 -0300

    modifying message to mach NS feel

--- ora_cs_customer.js
+++ ora_cs_customer.js
@@ -3,11 +3,11 @@
  * @NScriptType ClientScript
  * @NModuleScope SameAccount
  */
-define(['N/runtime', 'N/record'],
+define(['N/runtime', 'N/record', 'N/ui/message'],
 /**
  * @param {runtime} runtime
  */
-function(runtime, record) {
+function(runtime, record, ui_message) {
     
     /**
      * Function to be executed after page is initialized.
@@ -160,9 +160,22 @@ function(runtime, record) {
     		var customerName = currentRecord.getValue('entityid');
         	//log.debug('currentRecord', currentRecord);
         	//log.debug('customerName', customerName);
-    		var reminder = confirm('Please call ' + customerName + ' to welcome them as a new customer before saving the record. Click cancel to go back to the record');		
-        	return reminder;
-    	}
+    		//var reminder = confirm('Please call ' + customerName + ' to welcome them as a new customer before saving the record. Click cancel to go back to the record');		
+        	//return reminder;
+    		var vendorMessage = ui_message.create({
+    			title: "Customer contact reminder", 
+				message: 'Please call ' + customerName + ' to welcome them as a new customer before saving the record', 
+			    type: ui_message.Type.WARNING
+    		});
+    		//vendorMessage will disappear after 5s    		
+			var messagePromise = new Promise(function(resolve, reject) {
+    			vendorMessage.show();
+    			resolve(setTimeout(function(){vendorMessage.hide();alert('Verify alert message');}, 1500));
+    		});
+			return messagePromise.then(function() { 
+				return true; 
+			});
+    	}    	
     	else
     	{
         	log.debug('scriptParameter', 'Disabled');
