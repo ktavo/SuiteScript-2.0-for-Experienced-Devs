072df181988843747236ff713b0bc38fa7b0afb9
ktavo <gariosp@unal.edu.co>
2018-03-08 13:03:00 -0300

    Adding client side script to compare promise calls vs synchronous calls on client side

---
+++ ora_cs_promise_so.js
@@ -0,0 +1,39 @@
+/**
+ * @NApiVersion 2.x
+ * @NScriptType ClientScript
+ * @NModuleScope SameAccount
+ */
+define([
+	'N/record'
+],function(record) {
+    
+    /**
+     * Function to be executed after page is initialized.
+     *
+     * @param {Object} scriptContext
+     * @param {Record} scriptContext.currentRecord - Current form record
+     * @param {string} scriptContext.mode - The mode in which the record is being accessed (create, copy, or edit)
+     *
+     * @since 2015.2
+     */
+    function pageInit(scriptContext) {
+
+    }
+    
+    function promiseCall(var salesOrderId){
+    	
+    	
+    }
+
+    function nonPromiseCall(var salesOrderId){
+    	
+    	
+    }
+
+
+    return {
+        pageInit: pageInit,
+        
+    };
+    
+});
