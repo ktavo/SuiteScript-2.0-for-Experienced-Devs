89ce598159366f8fd7797e7b7248c98cb37722c4
ktavo <gariosp@unal.edu.co>
2018-06-05 11:09:25 -0300

    CoA purger

--- ora_mx_CoAPurge_scheduled.js
+++ ora_mx_CoAPurge_scheduled.js
@@ -1,91 +1,69 @@
 /**
  * @NApiVersion 2.0
- * @NScriptType Suitelet
+ * @NScriptType ScheduledScript
  * @NModuleScope SameAccount
  */
-define(['N/record', 'N/search', 'N/ui/serverWidget', 'N/runtime', 'N/redirect', 'N/task'],
+define(['N/record', 'N/search', 'N/task', 'N/runtime'],
 /**
  * @param {record} record
  * @param {search} search
- * @param {ui} ui
- * @param {email} email
- * @param {runtime} runtime
+ * @param {task} task
  */
-function(record, search, ui, runtime, redirect, task) {
+function(record, search, task, runtime) {
    
     /**
-     * Definition of the Suitelet script trigger point.
+     * Definition of the Scheduled script trigger point.
      *
-     * @param {Object} context
-     * @param {ServerRequest} context.request - Encapsulation of the incoming request
-     * @param {ServerResponse} context.response - Encapsulation of the Suitelet response
+     * @param {Object} scriptContext
+     * @param {string} scriptContext.type - The context in which the script is executed. It is one of the values from the scriptContext.InvocationType enum.
      * @Since 2015.2
      */
-	function onRequest(context) {
-        if (context.request.method === 'GET') {
-            var form = ui.createForm({
-                title: 'Suitelet Acount Purge'
-            });
-            form.addSubmitButton({
-                label: 'Purge CoA'
-            });
-            context.response.writePage(form);
-        } else {
-        	log.debug('*************', '************On POST Call***********');
-            var request = context.request;
-            var accountsArray = loadAndRunCoASearch();
-            deleteAccounts(accountsArray);          
-        }
+    function execute(scriptContext) {
+    	log.debug('*************', '************Called from suitelet***********');
+        var accountsArray = loadAndRunCoASearch();
+        deleteAccounts(accountsArray);          
     }
-	
-	function loadAndRunCoASearch() {
+    
+    function loadAndRunCoASearch() {
     	var accountArray = new Array();
-    	log.debug('ora_mx_CoAPurge.js', 'Running CoA search ....');
-        var mySearch = search.load({
+    	log.debug('ora_mx_CoAPurgeScheduled.js', 'Running CoA search ....');
+        var scheduledSearch = search.load({
             id: 'customsearch_mx_coa_purge'
         });
         var counter = 0;
-        mySearch.run().each(function(result) {
+        scheduledSearch.run().each(function(result) {
             //log.debug('Result: ', result);
             var accountId = result.id;
             accountArray.push(accountId);
             //log.debug('Search on entity', 'accountId: ' + accountId);            
-        	counter++;
-        	if (counter == 5)
-        	{
-        		//return false;
-        	}
             return true;
         });
         return accountArray;
 	}
-	   
+    
 	function deleteAccounts(accountsList){
-		log.debug('On delete accounts call', 'Account list size: ' + accountsList.length);
+		log.debug('On delete accoutns call', 'Account list size: ' + accountsList.length);
         while (accountsList.length > 0)
         {
-        	if (accountsList[0] == '10991' )
-        	{
-        		continue;
-        		
-        	}
         	try
         	{
+        		log.debug('On delete accoutns call', 'Inside while-try');
         		var remainingUsage = runtime.getCurrentScript().getRemainingUsage();
-        		if(remainingUsage < 200)
+        		log.debug('Checking remaining ussage', 'Ussage left:' + remainingUsage); 
+        		if(remainingUsage < 100 && remainingUsage > 85)
         		{
             		log.debug('Checking remaining ussage', 'Ussage left:' + remainingUsage); 
-            		callScheduledScript(accountsList.length);
-            		accountsList = [];
+            		//callMyself(accountsList.length);
             		break;
         		}
         		else
         		{
+            		log.debug('Tryinig to delete', 'Account ID:' + accountsList[0]); 
         			var deleteAccountRecord = record.delete({
                 		type: record.Type.ACCOUNT,
                 		id: accountsList[0],
                 	});
-            		//log.debug('Account Deletion successful', 'Account ID:' + accountsList[0]); 
+            		log.debug('Account Deletion successful', 'Account ID:' + accountsList[0]); 
             		accountsList.shift();
         		}
         	}
@@ -94,9 +72,10 @@ function(record, search, ui, runtime, redirect, task) {
         		if (reason.name == 'SSS_USAGE_LIMIT_EXCEEDED')
         		{
             		log.error('Account can\'t be deleted', 'Account ID: ' + accountsList[0] + ' Reason:' + reason.name);
-            		//callScheduledScript(accountsList.length);
+            		//callMyself(accountsList.length);
             		break;
         		}
+        		
         		else if (reason.name == 'THIS_RECORD_CANNOT_BE_DELETED_BECAUSE_IT_HAS_DEPENDENT_RECORDS')
         		{
             		log.error('Account can\'t be deleted', 'Account ID: '+  accountsList[0] + ' Reason:' + reason.name);
@@ -133,31 +112,10 @@ function(record, search, ui, runtime, redirect, task) {
         		}
         	}
         }
-        redirect.toSuitelet({
-            scriptId: 257,
-            deploymentId: 489,
-            parameters: {'':''} 
-        });
     }
-	
-	function callScheduledScript(accountsLeft)
-	{
-    	log.debug('Calling sheduled script', 'Account list size:' + accountsLeft);    	
-    	var scriptTask = task.create({taskType: task.TaskType.SCHEDULED_SCRIPT});
-    	scriptTask.scriptId = 259;	
-    	scriptTask.deploymentId = 'customdeploy_ora_mx_coapurge_scheduled';
-    	//scriptTask.params = {searchId: 'custsearch_456'};
-    	var scriptTaskId = scriptTask.submit();		
-	}
-	
+
     return {
-        onRequest: onRequest
+        execute: execute
     };
+    
 });
-
-
-
-
-
-
-
